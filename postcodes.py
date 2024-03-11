3f6a35d3c2f3214b12849339f3086bdc4e95ceed
Rahul <rbatra@hotmail.co.uk>
2024-03-11 17:13:25 +0000

    first successful request

--- postcodes.py
+++ postcodes.py
@@ -1,33 +1,36 @@
 import json
 import requests
 
-def get_json_data(url):               #retrieving data from a URL specified pn postcodes/docs
+# def get_json_data(url):               #retrieving data from a URL specified pn postcodes/docs
+#
+#     try:
+#         response = requests.get(url)
+#         json_data = response.json()
+#         return json_data
+#
+#         return json_data
+#     except requests.exceptions.RequestException as e:
+#         print(f"error found: {e}")
+#         return None
+#
+# postcode_api_url = "https://api.postcodes.io"
+# json_data = get_json_data(postcode_api_url)
+#
+#
+#
+#
+# if json_data:
+#     # Process the JSON data as needed
+#     print(json_data)
 
 
+response = requests.get('https://api.postcodes.io')
 
+if response.status_code == 200:
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        json_data = response.json()
-        return json_data
+    print(response.text)
 
+else:
+    print(f"error found: {e}")
 
-
-        return json_data
-    except requests.exceptions.RequestException as e:
-        print(f"error found: {e}")
-        return None
-
-
-
-
-postcode_api_url = "https://api.postcodes.io"
-json_data = get_json_data(postcode_api_url)
-
-
-
-
-if json_data:
-    # Process the JSON data as needed
-    print(json_data)
+# convert to a python dictionary
