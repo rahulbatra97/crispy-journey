8d0c46c8875a3d167da62551ac8ff50cde7ad075
Rahul <rbatra@hotmail.co.uk>
2024-03-12 11:32:19 +0000

    successful get request in a class

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# crispy-journey
-repo for the Postcode project with sparta global
+# Crispy-Journey
+## Repository for the postcode project (sparta global)
+
+Get and post requests, encode and decode from JSON 
+model the architect in an OOP fashion 
+each object has its own model. 
+
+
+Demonstrate at least two read communications and two create/update 
+communications with the postcodes API. Store the info from the API in appropriate objects and collections 
+where available. 
+
+
+
--- postcodes.py
+++ postcodes.py
@@ -1,36 +1,20 @@
 import json
 import requests
 
-# def get_json_data(url):               #retrieving data from a URL specified pn postcodes/docs
-#
-#     try:
-#         response = requests.get(url)
-#         json_data = response.json()
-#         return json_data
-#
-#         return json_data
-#     except requests.exceptions.RequestException as e:
-#         print(f"error found: {e}")
-#         return None
-#
-# postcode_api_url = "https://api.postcodes.io"
-# json_data = get_json_data(postcode_api_url)
-#
-#
-#
-#
-# if json_data:
-#     # Process the JSON data as needed
-#     print(json_data)
+
+class PostcodeApi:
+    def __init__(self):
+        pass
+
+    def get_postcode(self, postcode):
+
+        try:
+            response = requests.get(f'https://api.postcodes.io/postcodes/{postcode}'.format(postcode=postcode))
+            if response.status_code == 200:
+                return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"error found: {e}")
+            return None
 
 
-response = requests.get('https://api.postcodes.io')
-
-if response.status_code == 200:
-
-    print(response.text)
-
-else:
-    print(f"error found: {e}")
-
-# convert to a python dictionary
+print(PostcodeApi().get_postcode('SW112RG'))
