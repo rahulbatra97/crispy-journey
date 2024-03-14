69f62b4de84c37591529c0469ea314cdbb7ec99c
Rahul <rbatra@hotmail.co.uk>
2024-03-14 14:02:20 +0000

    successful POST method added

--- postcodes.py
+++ postcodes.py
@@ -9,12 +9,24 @@ class PostcodeApi:
     def get_postcode(self, postcode):
 
         try:
-            response = requests.get(f'https://api.postcodes.io/postcodes/{postcode}'.format(postcode=postcode))
-            if response.status_code == 200:
+            response = requests.get(f'https://api.postcodes.io/postcodes/{postcode}', )
+            if response.status_code == 200 or response.status_code == 201:
                 return response.json()
         except requests.exceptions.RequestException as e:
             print(f"error found: {e}")
             return None
 
+    def post_request_postcode(self, postcode_input):
+        """
+        :put in postcodes in a list format as postcode_input
+        """
+        response = requests.post(f'https://api.postcodes.io/postcodes',json={'postcodes': postcode_input})
+        return response.json()
 
-print(PostcodeApi().get_postcode('SW112RG'))
+post_code_api = PostcodeApi()
+
+print(post_code_api.post_request_postcode())
+
+
+#print(PostcodeApi().get_postcode('SW112RG'))
+# '.format(postcode=postcode
